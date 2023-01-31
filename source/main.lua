395210cee082b984d67b8b4a70b1e3f0488fa996
Maxime <maxrofra@gmail.com>
2023-01-31 18:15:36 +0000

    Initial commit

---
+++ source/main.lua
@@ -0,0 +1,16 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+
+local graphics <const> = playdate.graphics
+local sprite <const> = playdate.sprite
+
+function main()
+	print("Testing!")
+end
+
+main()
+
+function playdate.update()
+	--print("Update")
+end
